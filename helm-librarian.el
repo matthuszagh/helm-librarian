87462d07796db58e464d4f83bf976fba95d22637
Matt Huszagh <huszaghmatt@gmail.com>
2021-08-31 21:34:47 -0700

    add ability to open candidates

--- helm-librarian.el
+++ helm-librarian.el
@@ -12,11 +12,23 @@
   "~/library"
   "Library directory.")
 
-(setq librarian-executable "/home/matt/src/librarian/target/release/librarian")
-(setq librarian-library-directory "~/doc/library")
+;; TODO this should be something like "@title@ (@author@) etc." And it should be a defvar.
+(defun helm-librarian//candidate-display (candidate)
+  "Used to generate the string display for each resource in the helm buffer.
+CANDIDATE is a hash table corresponding to the json-parsed resource."
+  (gethash "title" candidate))
+
+;; TODO this should handle websites
+(defun helm-librarian//open-resource-file (candidate)
+  "Open the file corresponding to the resource CANDIDATE.
+The resource is formatted as a hash table as returned by `json-parse-string'."
+  (find-file (concat librarian-library-directory
+                     "/resources/"
+                     (elt (gethash "historical_checksums" candidate) 0))))
 
 (defun helm-librarian//candidates ()
-  "Helm candidates."
+  "Helm candidates.
+Returns an alist with the first item"
   (let ((json-parse-result
          (ignore-errors
            (json-parse-string
@@ -28,12 +40,12 @@
                      helm-pattern))))))
     (if json-parse-result
         (mapcar (lambda (result)
-                  (gethash "title" result))
+                  `(,(helm-librarian//candidate-display result) . ,result))
                 json-parse-result))))
 
 (defun helm-librarian//action (candidate)
   "Helm action performed when selecting CANDIDATE."
-  (message "%s" candidate))
+  (helm-librarian//open-resource-file candidate))
 
 (defun helm-librarian ()
   "Helm completions for librarian's search command."
