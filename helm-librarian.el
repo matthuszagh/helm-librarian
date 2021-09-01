711208da536b23a2d8949cef3963f6bb1082626c
Matt Huszagh <huszaghmatt@gmail.com>
2021-08-31 20:59:58 -0700

    separate candidates and action into functions

--- helm-librarian.el
+++ helm-librarian.el
@@ -15,38 +15,40 @@
 (setq librarian-executable "/home/matt/src/librarian/target/release/librarian")
 (setq librarian-library-directory "~/doc/library")
 
+(defun helm-librarian//candidates ()
+  "Helm candidates."
+  (let ((candidates nil)
+        (json-parse-result
+         (ignore-errors
+           (json-parse-string
+            (shell-command-to-string
+             (concat librarian-executable
+                     " -d "
+                     librarian-library-directory
+                     " search "
+                     helm-pattern))))))
+    (if json-parse-result
+        (progn
+          (let ((results (seq-into json-parse-result 'list)))
+            (dolist (result results)
+              (setq candidates
+                    (append
+                     candidates
+                     (list (gethash "title" result))))))))
+    candidates))
+
+(defun helm-librarian//action (candidate)
+  "Helm action performed when selecting CANDIDATE."
+  (message "%s" candidate))
+
 (defun helm-librarian ()
   "Helm completions for librarian's search command."
   (interactive)
   (helm
    :sources (helm-build-sync-source "librarian"
               :match-dynamic t
-              :candidates
-              (lambda ()
-                (let ((candidates nil)
-                      (json-parse-result
-                       (ignore-errors
-                         (json-parse-string
-                          (shell-command-to-string
-                           (concat librarian-executable
-                                   " -d "
-                                   librarian-library-directory
-                                   " search "
-                                   helm-pattern))))))
-                  (if json-parse-result
-                      (progn
-                        (let ((results (seq-into json-parse-result 'list)))
-                          (dolist (result results)
-                            (setq candidates
-                                  (append
-                                   candidates
-                                   (list (gethash "title" result))))))))
-                  candidates))
-
-              :action
-              (lambda (candidate)
-                (message "%s" candidate))
-
+              :candidates 'helm-librarian//candidates
+              :action 'helm-librarian//action
               :volatile t)
    :buffer "*helm librarian*"
    :prompt "librarian search: "))
